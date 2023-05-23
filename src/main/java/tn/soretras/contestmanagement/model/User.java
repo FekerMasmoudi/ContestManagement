8e24501c40e125a67653ab01f04b1d358041db38
FekerMasmoudi <feker.masmoudi@gmail.com>
2023-05-23 14:54:32 +0100

    add User class

---
+++ src/main/java/tn/soretras/contestmanagement/model/User.java
@@ -0,0 +1,125 @@
+package tn.soretras.contestmanagement.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ValueGenerationType;
+
+
+@Entity
+@Table
+
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id; 
+	@Column
+	private int cin;
+	@Column
+	private String fname;
+	@Column
+	private String lname;
+	@Column
+	private int phone;
+	@Column
+	private String username;
+	@Column
+	private String password;
+	@Column
+	private String email;
+	@Column
+	private Date birthday;
+	@Column
+	private String address;
+	
+	@ManyToOne(optional=false) //champ obligatoire
+	@JoinColumn(name="idrole", referencedColumnName="id")
+	private Role role;
+	
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public void setIdrole(int idrole) {
+		this.setIdrole(idrole); = idrole;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCin() {
+		return cin;
+	}
+	public void setCin(int cin) {
+		this.cin = cin;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", cin=" + cin + ", fname=" + fname + ", lname=" + lname + ", phone=" + phone
+				+ ", username=" + username + ", password=" + password + ", email=" + email + ", birthday=" + birthday
+				+ ", address=" + address + "]";
+	}	
+
+}
